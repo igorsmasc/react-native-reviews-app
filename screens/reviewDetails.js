78f3fb2746c2a26d800b27fec8228a06ff8743ad
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2020-06-19 02:40:35 -0300

    Data Between Screens

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function ReviewDetails() {
+export default function ReviewDetails({ route }) {
+    
+    const { title, body, rating } = route.params;
+
     return (
         <View style={globalStyles.container}>
-            <Text>Review Details Screen</Text>
+            <Text>{ title }</Text>
+            <Text>{ body }</Text>
+            <Text>{ rating }</Text>
+
         </View>
     )
 }
